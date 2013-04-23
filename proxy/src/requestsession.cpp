94e5ed7b58e0e52dfdc6c606752005e85410c28b
Justin Karneges <justin@affinix.com>
2013-04-23 16:22:01 -0700

    fix pushpin-check

--- proxy/src/requestsession.cpp
+++ proxy/src/requestsession.cpp
@@ -408,6 +408,8 @@ public:
 			"You should have received a copy of the GNU Affero General Public License\n"
 			"along with this program. If not, see <http://www.gnu.org/licenses/>.\n";
 
+			state = WaitingForResponse;
+
 			QMetaObject::invokeMethod(this, "respondSuccess", Qt::QueuedConnection, Q_ARG(QString, str));
 			return;
 		}
