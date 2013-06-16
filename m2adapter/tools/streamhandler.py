e5083a71d96ab65b747967c13dfee7cb1c809f5e
Justin Karneges <justin@affinix.com>
2013-06-15 23:03:17 -0700

    streaming file responder

---
+++ m2adapter/tools/streamhandler.py
@@ -0,0 +1,132 @@
+# this handler responds with a file
+
+import sys
+import os
+import tnetstring
+import zmq
+
+class Session(object):
+	def __init__(self):
+		self.file = None
+		self.out_seq = 0
+		self.credits = 0
+
+filename = sys.argv[1]
+
+sessions = dict()
+
+client_id = 'zhttp-test'
+
+ctx = zmq.Context()
+in_sock = ctx.socket(zmq.PULL)
+in_sock.bind('ipc:///tmp/zhttp-test-in')
+in_stream_sock = ctx.socket(zmq.DEALER)
+in_stream_sock.setsockopt(zmq.IDENTITY, client_id)
+in_stream_sock.bind('ipc:///tmp/zhttp-test-in-stream')
+out_sock = ctx.socket(zmq.PUB)
+out_sock.bind('ipc:///tmp/zhttp-test-out')
+
+poller = zmq.core.poll.Poller()
+poller.register(in_sock, zmq.POLLIN)
+poller.register(in_stream_sock, zmq.POLLIN)
+
+while True:
+	socks = dict(poller.poll())
+	if socks.get(in_sock) == zmq.POLLIN:
+		m_raw = in_sock.recv()
+		req = tnetstring.loads(m_raw)
+		print 'IN %s' % req
+
+		id = req['id']
+		if id in sessions:
+			print 'session already active'
+			continue
+
+		if 'type' in req:
+			print 'wrong packet type'
+			continue
+
+		s = Session()
+		sessions[id] = s
+
+		s.file = open(filename, 'r')
+
+		if 'credits' in req:
+			s.credits += req['credits']
+
+		body = ''
+		eof = False
+		while s.credits > 0:
+			buf = s.file.read(s.credits)
+			s.credits -= len(buf)
+			body += buf
+			if len(buf) == 0:
+				eof = True
+				break
+
+		resp = dict()
+		resp['from'] = client_id
+		resp['id'] = req['id']
+		resp['seq'] = s.out_seq
+		resp['code'] = 200
+		resp['reason'] = 'OK'
+		headers = list()
+		headers.append(['Content-Type', 'text/plain'])
+		headers.append(['Content-Length', str(os.stat(filename).st_size)])
+		resp['headers'] = headers
+		if len(body) > 0:
+			resp['body'] = body
+		if not eof:
+			resp['more'] = True
+
+		s.out_seq += 1
+
+		if eof:
+			del sessions[id]
+
+		print 'OUT %s' % resp
+		out_sock.send(req['from'] + ' ' + tnetstring.dumps(resp))
+	elif socks.get(in_stream_sock) == zmq.POLLIN:
+		parts = in_stream_sock.recv_multipart()
+		req = tnetstring.loads(parts[1])
+		print 'IN stream %s' % req
+
+		# we are only streaming output, so subsequent input messages must be credits
+		if 'type' not in req or req['type'] != 'credit':
+			print 'wrong packet type'
+			continue
+
+		id = req['id']
+
+		s = sessions[id]
+
+		if 'credits' in req:
+			s.credits += req['credits']
+
+		body = ''
+		eof = False
+		while s.credits > 0:
+			buf = s.file.read(s.credits)
+			s.credits -= len(buf)
+			body += buf
+			if len(buf) == 0:
+				eof = True
+				break
+
+		resp = dict()
+		resp['from'] = client_id
+		resp['id'] = id
+		resp['seq'] = s.out_seq
+
+		s.out_seq += 1
+
+		if len(body) > 0:
+			resp['body'] = body
+		if not eof:
+			resp['more'] = True
+
+		if eof:
+			del sessions[id]
+
+		print 'OUT %s' % resp
+		out_sock.send(req['from'] + ' ' + tnetstring.dumps(resp))
