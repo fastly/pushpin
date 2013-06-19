3d7aa21c912c26d8d5b5ea70515937d6940849d1
Justin Karneges <justin@affinix.com>
2013-06-19 00:15:29 -0700

    handy mongrel2 control client

---
+++ m2adapter/tools/m2control.py
@@ -0,0 +1,25 @@
+# adapted from mongrel2's manual
+
+import sys
+import zmq
+import tnetstring
+from pprint import pprint
+  
+CTX = zmq.Context()
+addr = sys.argv[1]
+  
+ctl = CTX.socket(zmq.REQ)
+  
+print "CONNECTING"
+ctl.connect(addr)
+  
+while True:
+	cmd = raw_input("> ")
+	# will only work with simple commands that have no arguments
+	ctl.send(tnetstring.dumps([cmd, {}]))
+
+	resp = ctl.recv()
+
+	pprint(tnetstring.loads(resp))
+
+ctl.close()
