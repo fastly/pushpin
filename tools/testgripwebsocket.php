f66eecb8d2de34daf1d176a136c744d7f8564ac4
Justin Karneges <justin@affinix.com>
2014-09-10 10:26:41 -0700

    ridiculousness

---
+++ tools/testgripwebsocket.php
@@ -0,0 +1,19 @@
+<?php
+
+$body = @file_get_contents('php://input');
+
+if (substr($body, 0, 6) === "OPEN\r\n") {
+    header('Sec-WebSocket-Extensions:grip; message-prefix=""');
+    $sub = True;
+} else {
+    $sub = False;
+}
+
+print $body;
+
+if ($sub) {
+    $msg = 'c:{"type": "subscribe", "channel": "test"}';
+    print 'TEXT ' . dechex(strlen($msg)) . "\r\n" . $msg . "\r\n";
+}
+
+?>
