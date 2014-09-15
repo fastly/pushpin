0770082ad969b21a237833f52633f2855dcafb86
Justin Karneges <justin@affinix.com>
2014-09-15 09:29:43 -0700

    a package script

---
+++ package.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+set -e
+
+if [ $# -lt 1 ]; then
+	echo "usage: $0 [version]"
+	exit 1
+fi
+
+VERSION=$1
+
+mkdir -p build/pushpin-$VERSION
+cp -a .gitignore common config COPYING doc handler internal.conf m2adapter Makefile proxy pushpin qzmq README.md runner tools build/pushpin-$VERSION
+rm -rf build/pushpin-$VERSION/qzmq/.git build/pushpin-$VERSION/common/.git
+cd build
+tar jcvf pushpin-$VERSION.tar.bz2 pushpin-$VERSION
