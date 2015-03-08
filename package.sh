7c2fff78a615c9522c8dc20a8826e65518e48581
Justin Karneges <justin@affinix.com>
2015-03-08 15:43:27 -0700

    update packaging file

--- package.sh
+++ package.sh
@@ -9,7 +9,7 @@ fi
 VERSION=$1
 
 mkdir -p build/pushpin-$VERSION
-cp -a .gitignore common config COPYING doc handler m2adapter Makefile proxy pushpin qzmq README.md runner tools build/pushpin-$VERSION
+cp -a .gitignore common COPYING doc examples handler init.sh m2adapter Makefile proxy pushpin qzmq README.md runner tools build/pushpin-$VERSION
 rm -rf build/pushpin-$VERSION/qzmq/.git build/pushpin-$VERSION/common/.git
 cd build
 tar jcvf pushpin-$VERSION.tar.bz2 pushpin-$VERSION
