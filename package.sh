323470e117abb5bd042da1c9bd71304f594611c7
Justin Karneges <justin@affinix.com>
2016-02-20 22:36:35 -0800

    update package script

--- package.sh
+++ package.sh
@@ -9,7 +9,7 @@ fi
 VERSION=$1
 
 mkdir -p build/pushpin-$VERSION
-cp -a .gitignore CHANGELOG.md configure COPYING corelib doc examples handler m2adapter proxy pushpin pushpin.pro pushpin.qc README.md runner tools build/pushpin-$VERSION
+cp -a .gitignore CHANGELOG.md configure COPYING doc examples pushpin pushpin.pro pushpin.qc README.md runner tools build/pushpin-$VERSION
 rm -rf build/pushpin-$VERSION/corelib/qzmq/.git build/pushpin-$VERSION/corelib/common/.git
 echo $VERSION > build/pushpin-$VERSION/version
 cd build
