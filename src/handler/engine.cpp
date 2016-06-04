60e0463acdac576ccc909723bcb428128c4d25f6
Justin Karneges <justin@karneges.com>
2016-06-04 10:57:17 -0700

    non-hold responses need similar header filtering

--- src/handler/engine.cpp
+++ src/handler/engine.cpp
@@ -398,13 +398,6 @@ public:
 
 	void start()
 	{
-		// don't relay these headers. their meaning is handled by
-		//   zurl and they only apply to the outgoing hop.
-		response.headers.removeAll("Connection");
-		response.headers.removeAll("Keep-Alive");
-		response.headers.removeAll("Content-Encoding");
-		response.headers.removeAll("Transfer-Encoding");
-
 		if(mode == Instruct::ResponseHold)
 		{
 			// set timeout
@@ -1228,6 +1221,13 @@ private:
 			setFinished(true);
 			return;
 		}
+
+		// don't relay these headers. their meaning is handled by
+		//   zurl and they only apply to the outgoing hop.
+		instruct.response.headers.removeAll("Connection");
+		instruct.response.headers.removeAll("Keep-Alive");
+		instruct.response.headers.removeAll("Content-Encoding");
+		instruct.response.headers.removeAll("Transfer-Encoding");
 
 		if(instruct.holdMode == Instruct::NoHold)
 		{
