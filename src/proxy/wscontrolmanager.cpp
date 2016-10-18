a6a59d65a5085159aee8631a106e7652e82b7fcb
Justin Karneges <justin@karneges.com>
2016-10-17 22:49:43 -0700

    wscontrol: smooth out keep-alives

--- src/proxy/wscontrolmanager.cpp
+++ src/proxy/wscontrolmanager.cpp
@@ -21,6 +21,7 @@
 
 #include <assert.h>
 #include <QPointer>
+#include <QDateTime>
 #include <QTimer>
 #include "qzmqsocket.h"
 #include "qzmqvalve.h"
@@ -30,8 +31,13 @@
 #include "wscontrolsession.h"
 
 #define DEFAULT_HWM 101000
-#define SESSION_TTL 60
-#define SESSION_REFRESH (SESSION_TTL * 3 / 4)
+
+#define REFRESH_INTERVAL 1000
+#define SESSION_EXPIRE 60000
+
+#define SESSION_SHOULD_PROCESS (SESSION_EXPIRE * 3 / 4)
+#define SESSION_MUST_PROCESS (SESSION_EXPIRE * 4 / 5)
+#define SESSION_REFRESH_BUCKETS (SESSION_SHOULD_PROCESS / REFRESH_INTERVAL)
 
 #define PACKET_ITEMS_MAX 128
 
@@ -40,6 +46,14 @@ class WsControlManager::Private : public QObject
 	Q_OBJECT
 
 public:
+	class KeepAliveRegistration
+	{
+	public:
+		WsControlSession *s;
+		qint64 lastRefresh;
+		int refreshBucket;
+	};
+
 	WsControlManager *q;
 	int ipcFileMode;
 	QString inSpec;
@@ -48,8 +62,11 @@ public:
 	QZmq::Socket *outSock;
 	QZmq::Valve *inValve;
 	QHash<QByteArray, WsControlSession*> sessionsByCid;
-	QTimer *keepAliveTimer;
-	QSet<WsControlSession*> keepAliveSessions;
+	QTimer *refreshTimer;
+	QHash<WsControlSession*, KeepAliveRegistration*> keepAliveRegistrations;
+	QMap<QPair<qint64, KeepAliveRegistration*>, KeepAliveRegistration*> sessionsByLastRefresh;
+	QSet<KeepAliveRegistration*> sessionRefreshBuckets[SESSION_REFRESH_BUCKETS];
+	int currentSessionRefreshBucket;
 
 	Private(WsControlManager *_q) :
 		QObject(_q),
@@ -57,17 +74,21 @@ public:
 		ipcFileMode(-1),
 		inSock(0),
 		outSock(0),
-		inValve(0)
+		inValve(0),
+		currentSessionRefreshBucket(0)
 	{
-		 keepAliveTimer = new QTimer(this);
-		 connect(keepAliveTimer, &QTimer::timeout, this, &Private::keepAlive_timeout);
+		refreshTimer = new QTimer(this);
+		connect(refreshTimer, &QTimer::timeout, this, &Private::refresh_timeout);
 	}
 
 	~Private()
 	{
-		keepAliveTimer->disconnect(this);
-		keepAliveTimer->setParent(0);
-		keepAliveTimer->deleteLater();
+		assert(sessionsByCid.isEmpty());
+		assert(keepAliveRegistrations.isEmpty());
+
+		refreshTimer->disconnect(this);
+		refreshTimer->setParent(0);
+		refreshTimer->deleteLater();
 	}
 
 	bool setupIn()
@@ -112,6 +133,22 @@ public:
 		return true;
 	}
 
+	int smallestSessionRefreshBucket()
+	{
+		int best = -1;
+		int bestSize;
+		for(int n = 0; n < SESSION_REFRESH_BUCKETS; ++n)
+		{
+			if(best == -1 || sessionRefreshBuckets[n].count() < bestSize)
+			{
+				best = n;
+				bestSize = sessionRefreshBuckets[n].count();
+			}
+		}
+
+		return best;
+	}
+
 	void write(const WsControlPacket &packet)
 	{
 		assert(outSock);
@@ -132,15 +169,49 @@ public:
 		write(out);
 	}
 
+	void registerKeepAlive(WsControlSession *s)
+	{
+		if(keepAliveRegistrations.contains(s))
+			return;
+
+		qint64 now = QDateTime::currentMSecsSinceEpoch();
+
+		KeepAliveRegistration *r = new KeepAliveRegistration;
+		r->s = s;
+		keepAliveRegistrations.insert(s, r);
+
+		r->lastRefresh = now;
+		sessionsByLastRefresh.insert(QPair<qint64, KeepAliveRegistration*>(r->lastRefresh, r), r);
+
+		r->refreshBucket = smallestSessionRefreshBucket();
+		sessionRefreshBuckets[r->refreshBucket] += r;
+
+		setupKeepAlive();
+	}
+
+	void unregisterKeepAlive(WsControlSession *s)
+	{
+		KeepAliveRegistration *r = keepAliveRegistrations.value(s);
+		if(!r)
+			return;
+
+		sessionRefreshBuckets[r->refreshBucket].remove(r);
+		sessionsByLastRefresh.remove(QPair<qint64, KeepAliveRegistration*>(r->lastRefresh, r));
+		keepAliveRegistrations.remove(s);
+		delete r;
+
+		setupKeepAlive();
+	}
+
 	void setupKeepAlive()
 	{
-		if(!keepAliveSessions.isEmpty())
+		if(!keepAliveRegistrations.isEmpty())
 		{
-			if(!keepAliveTimer->isActive())
-				keepAliveTimer->start(SESSION_REFRESH * 1000);
+			if(!refreshTimer->isActive())
+				refreshTimer->start(REFRESH_INTERVAL);
 		}
 		else
-			keepAliveTimer->stop();
+			refreshTimer->stop();
 	}
 
 private slots:
@@ -197,16 +268,55 @@ private slots:
 		}
 	}
 
-	void keepAlive_timeout()
+	void refresh_timeout()
 	{
+		qint64 now = QDateTime::currentMSecsSinceEpoch();
+
 		WsControlPacket packet;
 
-		foreach(WsControlSession *s, keepAliveSessions)
+		// process the current bucket
+		const QSet<KeepAliveRegistration*> &bucket = sessionRefreshBuckets[currentSessionRefreshBucket];
+		foreach(KeepAliveRegistration *r, bucket)
 		{
+			// move to the end
+			QPair<qint64, KeepAliveRegistration*> k(r->lastRefresh, r);
+			sessionsByLastRefresh.remove(k);
+			r->lastRefresh = now;
+			sessionsByLastRefresh.insert(QPair<qint64, KeepAliveRegistration*>(r->lastRefresh, r), r);
+
 			WsControlPacket::Item i;
-			i.cid = s->cid();
+			i.cid = r->s->cid();
 			i.type = WsControlPacket::Item::KeepAlive;
-			i.ttl = SESSION_TTL;
+			i.ttl = SESSION_EXPIRE / 1000;
+			packet.items += i;
+
+			// if we're at max, send out now
+			if(packet.items.count() >= PACKET_ITEMS_MAX)
+			{
+				write(packet);
+				packet.items.clear();
+			}
+		}
+
+		// process any others
+		qint64 threshold = now - SESSION_MUST_PROCESS;
+		while(!sessionsByLastRefresh.isEmpty())
+		{
+			QMap<QPair<qint64, KeepAliveRegistration*>, KeepAliveRegistration*>::iterator it = sessionsByLastRefresh.begin();
+			KeepAliveRegistration *r = it.value();
+
+			if(r->lastRefresh > threshold)
+				break;
+
+			// move to the end
+			sessionsByLastRefresh.erase(it);
+			r->lastRefresh = now;
+			sessionsByLastRefresh.insert(QPair<qint64, KeepAliveRegistration*>(r->lastRefresh, r), r);
+
+			WsControlPacket::Item i;
+			i.cid = r->s->cid();
+			i.type = WsControlPacket::Item::KeepAlive;
+			i.ttl = SESSION_EXPIRE / 1000;
 			packet.items += i;
 
 			// if we're at max, send out now
@@ -220,6 +330,10 @@ private slots:
 		// send the rest
 		if(!packet.items.isEmpty())
 			write(packet);
+
+		++currentSessionRefreshBucket;
+		if(currentSessionRefreshBucket >= SESSION_REFRESH_BUCKETS)
+			currentSessionRefreshBucket = 0;
 	}
 };
 
@@ -282,14 +396,12 @@ void WsControlManager::write(const WsControlPacket::Item &item)
 
 void WsControlManager::registerKeepAlive(WsControlSession *s)
 {
-	d->keepAliveSessions += s;
-	d->setupKeepAlive();
+	d->registerKeepAlive(s);
 }
 
 void WsControlManager::unregisterKeepAlive(WsControlSession *s)
 {
-	d->keepAliveSessions -= s;
-	d->setupKeepAlive();
+	d->unregisterKeepAlive(s);
 }
 
 #include "wscontrolmanager.moc"
