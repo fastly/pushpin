59ae1d9039c90886131c712a886a0b91ccdaa6d9
Justin Karneges <justin@karneges.com>
2017-01-17 22:56:22 -0800

    update year in copyright notice

--- src/proxy/requestsession.cpp
+++ src/proxy/requestsession.cpp
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2012-2016 Fanout, Inc.
+ * Copyright (C) 2012-2017 Fanout, Inc.
  *
  * This file is part of Pushpin.
  *
@@ -299,7 +299,7 @@ public:
 		if(requestData.headers.contains("Pushpin-Check"))
 		{
 			QString str =
-			"Copyright (C) 2012-2016 Fanout, Inc.\n"
+			"Copyright (C) 2012-2017 Fanout, Inc.\n"
 			"\n"
 			"Pushpin is free software: you can redistribute it and/or modify it under\n"
 			"the terms of the GNU Affero General Public License as published by the Free\n"
