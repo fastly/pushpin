1c8e900c2fdc4f5d87f79e01845315cb558d5fe4
Justin Karneges <justin@karneges.com>
2019-03-25 13:35:57 -0700

    whitespace nit

--- src/corelib/zwebsocket.cpp
+++ src/corelib/zwebsocket.cpp
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2014-2016 Fanout, Inc.
+ * Copyright (C) 2014-2019 Fanout, Inc.
  *
  * This file is part of Pushpin.
  *
@@ -742,7 +742,7 @@ public:
 		out.from = manager->instanceId();
 		out.ids += ZhttpResponsePacket::Id(rid.second, outSeq++);
 		out.userData = userData;
-		
+
 		manager->writeWs(out, rid.first);
 	}
 
