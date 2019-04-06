fcaa8adb075d6cb3edd9cf9eee7d9854741820fd
Justin Karneges <justin@karneges.com>
2019-04-06 10:57:45 -0700

    Revert "response holds without prev-id should not be triggerable for recovery"
    This reverts commit 8d1f7fd924751d7eb34f84a13034abb0fcf92bd1.

--- src/handler/httpsession.cpp
+++ src/handler/httpsession.cpp
@@ -349,22 +349,9 @@ public:
 
 		needUpdate = false;
 
-		bool havePrevId = false;
-		QHashIterator<QString, Instruct::Channel> it(channels);
-		while(it.hasNext())
+		if(instruct.holdMode != Instruct::ResponseHold && nextUri.isEmpty())
 		{
-			it.next();
-			const Instruct::Channel &c = it.value();
-			if(!c.prevId.isNull())
-			{
-				havePrevId = true;
-				break;
-			}
-		}
-
-		if(!havePrevId || (instruct.holdMode != Instruct::ResponseHold && nextUri.isEmpty()))
-		{
-			// can't update without prevId and valid link
+			// can't update without valid link
 			return;
 		}
 
