a1fe6353cfa03abdc2afc569126b5a7f37bcdb60
Justin Karneges <justin@karneges.com>
2021-09-01 11:04:21 -0700

    command.py: port to python3

--- tools/command.py
+++ tools/command.py
@@ -5,39 +5,49 @@ import tnetstring
 import zmq
 
 def make_tnet_compat(obj):
-	if isinstance(obj, dict):
-		out = dict()
-		for k, v in obj.iteritems():
-			out[make_tnet_compat(k)] = make_tnet_compat(v)
-		return out
-	elif isinstance(obj, list):
-		out = list()
-		for v in obj:
-			out.append(make_tnet_compat(v))
-		return out
-	elif isinstance(obj, unicode):
-		return obj.encode('utf-8')
-	else:
-		return obj
+    if isinstance(obj, dict):
+        out = {}
+        for k, v in obj.items():
+            out[make_tnet_compat(k)] = make_tnet_compat(v)
+        return out
+    elif isinstance(obj, list):
+        out = list()
+        for v in obj:
+            out.append(make_tnet_compat(v))
+        return out
+    elif isinstance(obj, str):
+        return obj.encode('utf-8')
+    else:
+        return obj
 
 ctx = zmq.Context()
 sock = ctx.socket(zmq.REQ)
 sock.connect(sys.argv[1])
 
-req = dict()
-req['id'] = str(uuid.uuid4())
-req['method'] = sys.argv[2]
+method = sys.argv[2]
+
 if len(sys.argv) > 3:
-	args = json.loads(sys.argv[3])
-	assert(isinstance(args, dict))
-	req['args'] = make_tnet_compat(args)
+    args = json.loads(sys.argv[3])
+    assert(isinstance(args, dict))
 else:
-	req['args'] = dict()
-print 'calling %s: args=%s' % (req['method'], req['args'])
+    args = {}
+
+print('calling {}: args={}'.format(method, repr(args)))
+
+req = {
+    b'id': str(uuid.uuid4()).encode('utf-8'),
+    b'method': method.encode('utf-8'),
+    b'args': make_tnet_compat(args)
+}
+
 sock.send(tnetstring.dumps(req))
 
 resp = tnetstring.loads(sock.recv())
-if resp['success']:
-	print 'success: %s' % resp['value']
+
+if resp[b'success']:
+    value = resp[b'value']
+    print('success: {}'.format(repr(value)))
 else:
-	print 'error: %s' % resp['condition']
+    condition = resp[b'condition'].decode('utf-8')
+    value = resp.get(b'value')
+    print('error: {} {}'.format(condition, repr(value)))
