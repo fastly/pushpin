7f7dc85ae0c5bd9c2a835f0f653af0987e16af10
Frederik Deweerdt <fdeweerdt@fastly.com>
2021-12-17 09:54:03 -0800

    network isolation, dedicated group for access to the socket

--- fastly-build/deb-postinstall.sh
+++ fastly-build/deb-postinstall.sh
@@ -3,11 +3,13 @@
 set -x -e
 
 groupadd pushpin || true
-useradd -g pushpin pushpin || true
+groupadd pushpin-listener || true
+useradd -g pushpin -G pushpin-listener pushpin || true
 mkdir -p /var/run/pushpin
 chown pushpin:pushpin /var/run/pushpin
 
 cp /opt/fst-pushpin/etc/pushpin.service /etc/systemd/system
+cp /opt/fst-pushpin/etc/pushpin-socat.service /etc/systemd/system
 
 # Reload systemd so that it picks up the packaged unit fragment file.
 # The query `systemctl is-system-running` exits with a non-0 status and it says
