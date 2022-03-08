a6abed73c528d9b61f2846b5cad3651a61a67ef6
Frederik Deweerdt <fdeweerdt@fastly.com>
2022-03-08 12:49:21 -0800

    fix group

--- fastly-build/deb-postinstall.sh
+++ fastly-build/deb-postinstall.sh
@@ -7,7 +7,7 @@ groupadd pushpin-listener || true
 useradd -r -g pushpin -G pushpin-listener pushpin || true
 mkdir -p /var/run/pushpin
 chown pushpin:pushpin /var/run/pushpin
-chown pushpin:pushpin /opt/fst-pushpin/bin/pushpin-healthcheck
+chown pushpin:pushpin-listener /opt/fst-pushpin/bin/pushpin-healthcheck
 chmod g+s /opt/fst-pushpin/bin/pushpin-healthcheck
 
 cp /opt/fst-pushpin/etc/pushpin.service /etc/systemd/system
