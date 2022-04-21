3d837d6f92843c7fe0d2b6b308014961f99d24f9
Frederik Deweerdt <fdeweerdt@fastly.com>
2022-04-20 19:15:08 -0700

    Iptables rules: fix the name of the ipv6 rule, allow dns requests and connections to 7999

--- scripts/pushpin-iptables.sh
+++ scripts/pushpin-iptables.sh
@@ -15,12 +15,14 @@ setup() {
         iptables -I PUSHPINOUT -j ACCEPT
         iptables -I PUSHPINOUT -m set --match-set proxyb_block_egress dst -j DROP
         iptables -I PUSHPINOUT -m set --match-set proxyb_infra_blocked dst -j REJECT || echo >&2 "WARNING: Infra block rule skipped for IPv4"
+        iptables -I PUSHPINOUT -p udp --dport 53 -s 127.0.0.1 -d 127.0.0.1  -j ACCEPT
+        iptables -I PUSHPINOUT -p tcp --sport 7999 -s 127.0.0.1 -d 127.0.0.1 -j ACCEPT
 
         ip6tables -N PUSHPIN6OUT
         ip6tables -I OUTPUT -m owner --uid-owner pushpin -g PUSHPIN6OUT
-        ip6tables -I PROXYB6OUT -j ACCEPT
-        ip6tables -I PROXYB6OUT -m set --match-set proxyb_block_egress6 dst -j DROP
-        ip6tables -I PROXYB6OUT -m set --match-set proxyb_infra_blocked6 dst -j REJECT || echo >&2 "WARNING: Infra block rule skipped for IPv6"
+        ip6tables -I PUSHPIN6OUT -j ACCEPT
+        ip6tables -I PUSHPIN6OUT -m set --match-set proxyb_block_egress6 dst -j DROP
+        ip6tables -I PUSHPIN6OUT -m set --match-set proxyb_infra_blocked6 dst -j REJECT || echo >&2 "WARNING: Infra block rule skipped for IPv6"
 }
 
 cleanup() {
