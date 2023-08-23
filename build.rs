4228b371e2dd10d769c578c2438d102ecb545704
Sima <64804941+sima-fastly@users.noreply.github.com>
2023-08-23 15:19:50 -0700

    Runner config file processing in Rust (#47722)
    Rust re-write of Runner config file processing

--- build.rs
+++ build.rs
@@ -3,11 +3,12 @@ use std::fs;
 use std::io::{BufRead, BufReader};
 
 fn main() -> Result<(), Box<dyn Error>> {
-    let app_version = {
+    let (app_version, config_dir) = {
         let f = fs::File::open("conf.pri")?;
         let reader = BufReader::new(f);
 
-        let mut s = String::new();
+        let mut av = String::new();
+        let mut cd = String::new();
 
         for line in reader.lines() {
             let line = line?;
@@ -15,14 +16,19 @@ fn main() -> Result<(), Box<dyn Error>> {
             if line.starts_with("APP_VERSION =") {
                 let pos = line.find('=').unwrap();
 
-                s = line[(pos + 1)..].trim().into();
+                av = line[(pos + 1)..].trim().into();
+            } else if line.starts_with("CONFIGDIR =") {
+                let pos = line.find('=').unwrap();
+
+                cd = line[(pos + 1)..].trim().into();
             }
         }
 
-        s
+        (av, cd)
     };
 
     println!("cargo:rustc-env=APP_VERSION={}", app_version);
+    println!("cargo:rustc-env=CONFIG_DIR={}", config_dir);
 
     Ok(())
 }
