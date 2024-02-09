a3608de7612fbf6c09d88320c5a03756821fc32e
Justin Karneges <jkarneges@fastly.com>
2024-02-08 16:06:59 -0800

    fix some c++ warnings

---
+++ src/cpp/qtcompat.h
@@ -0,0 +1,42 @@
+/*
+ * Copyright (C) 2024 Fastly, Inc.
+ *
+ * This file is part of Pushpin.
+ *
+ * $FANOUT_BEGIN_LICENSE:APACHE2$
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * $FANOUT_END_LICENSE$
+ */
+
+#include <QMetaType>
+#include <QVariant>
+
+inline QMetaType::Type typeId(const QVariant &v)
+{
+#if QT_VERSION >= 0x060000
+	return (QMetaType::Type)v.typeId();
+#else
+	return (QMetaType::Type)v.type();
+#endif
+}
+
+inline bool canConvert(const QVariant &v, QMetaType::Type type)
+{
+#if QT_VERSION >= 0x060000
+    return v.canConvert(QMetaType(type));
+#else
+    return v.canConvert(type);
+#endif
+}
