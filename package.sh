3c8569497c1c2bc52cc0f7032f85eec3844dbdca
Justin Karneges <jkarneges@fastly.com>
2024-07-18 11:12:13 -0700

    include cbindgen.toml when packaging (#48048)

--- package.sh
+++ package.sh
@@ -12,7 +12,7 @@ DESTDIR=build/pushpin-$VERSION
 
 mkdir -p $DESTDIR
 
-cp -a .gitignore benches build.rs Cargo.lock Cargo.toml CHANGELOG.md examples LICENSE Makefile postbuild SECURITY.md README.md src tools $DESTDIR
+cp -a .gitignore benches build.rs Cargo.lock Cargo.toml cbindgen.toml CHANGELOG.md examples LICENSE Makefile postbuild SECURITY.md README.md src tools $DESTDIR
 
 sed -i.orig -e "s/^version = .*/version = \"$VERSION\"/g" $DESTDIR/Cargo.toml
 rm $DESTDIR/Cargo.toml.orig
