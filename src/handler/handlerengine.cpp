a9052b5b0de6c659472c9f49e735a83cbac914f1
maksymkei <111739434+maksymkeihz@users.noreply.github.com>
2025-02-25 17:29:59 +0200

    Update handlerengine.cpp

--- src/handler/handlerengine.cpp
+++ src/handler/handlerengine.cpp
@@ -1362,6 +1362,7 @@ public:
 		zhttpIn->setInstanceId(config.instanceId);
 		zhttpIn->setServerInStreamSpecs(config.serverInStreamSpecs);
 		zhttpIn->setServerOutSpecs(config.serverOutSpecs);
+		zhttpIn->setCacheEnable(config.cacheEnable);
 
 		zhttpOut = new ZhttpManager(this);
 		zhttpOut->setInstanceId(config.instanceId);
