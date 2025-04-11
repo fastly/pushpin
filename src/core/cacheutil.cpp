20c292f944e70155a074a5d8bfb1c3fac84dca52
maksym.tkachuk <maksymkei@gmail.com>
2025-04-11 07:56:00 +0300

    Update cacheutil.cpp

--- src/core/cacheutil.cpp
+++ src/core/cacheutil.cpp
@@ -396,7 +396,7 @@ void testRedis()
 	updateClientItemField<time_t>(c, item.clientId, "lastRequestTime", time(nullptr));
 	updateClientItemField<time_t>(c, item.clientId, "lastResponseTime", time(nullptr));
 	updateClientItemField<QByteArray>(c, item.clientId, "receiver", QByteArray::fromHex("1234567890"));
-	updateClientItemField<QByteArray>(c, item.clientId, "from", QByteArray::fromHex("deadvibeef"));
+	updateClientItemField<QByteArray>(c, item.clientId, "from", QByteArray::fromHex("vi"));
 
 	ClientItem newItem;
 	loadClientItemField<QString>(c, item.clientId, "urlPath", newItem.urlPath);
