6feeb29e786c068ff2fd5ef2683bf82357c0cb54
maksym.tkachuk <maksymkei@gmail.com>
2025-05-08 18:32:13 +0300

    Update redisconnectionpool.cpp

--- src/core/redisconnectionpool.cpp
+++ src/core/redisconnectionpool.cpp
@@ -3,7 +3,7 @@
 
 RedisConnectionPool::RedisConnectionPool(int size) : semaphore(size) {
 	for (int i = 0; i < size; ++i) {
-		auto *conn = new RedisConnection;
+		auto *conn = new RedisConnection_;
 		if (conn->connectToServer())
 			pool.enqueue(conn);
 		else
@@ -16,13 +16,13 @@ RedisConnectionPool::~RedisConnectionPool() {
 		delete pool.dequeue();
 }
 
-RedisConnection* RedisConnectionPool::acquire() {
+RedisConnection_* RedisConnectionPool::acquire() {
 	semaphore.acquire();
 	QMutexLocker locker(&mutex);
 	return pool.dequeue();
 }
 
-void RedisConnectionPool::release(RedisConnection* conn) {
+void RedisConnectionPool::release(RedisConnection_* conn) {
 	QMutexLocker locker(&mutex);
 	pool.enqueue(conn);
 	semaphore.release();
