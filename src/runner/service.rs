0cebe61f52ec5ece78409df344ca622f929050c0
maksym.tkachuk <maksymkei@gmail.com>
2025-05-23 20:04:07 +0300

    Update service.rs

--- src/runner/service.rs
+++ src/runner/service.rs
@@ -452,8 +452,9 @@ fn log_message(name: &str, level: log::Level, msg: &str) {
         msg.to_string()
     };
 
-    if level == log::Level::Trace
-        return
+    if level == log::Level::Trace {
+        return;
+    }
 
     log::logger().log(
         &log::Record::builder()
