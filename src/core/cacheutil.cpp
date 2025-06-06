78842393cf68f8fe5b1c82621fbb489da9c1b226
maksym.tkachuk <maksymkei@gmail.com>
2025-06-06 15:37:49 +0300

    Update cacheutil.cpp

--- src/core/cacheutil.cpp
+++ src/core/cacheutil.cpp
@@ -1652,11 +1652,13 @@ int parse_packet_msg(Scheme scheme, const ZhttpRequestPacket& packet, PacketMsg&
 	else
 	{
 		QString subKey = QString("WS+");
+		/*
 		if (is_subscribe_method(packetMsg.method))
 		{
 			subKey += instanceId.data();
 			log_debug("[QQQ] %s", qPrintable(subKey));
 		}
+		*/
 		packetMsg.paramsHash = build_hash_key(jsonMap, subKey);
 	}
 	packetMsg.subscription = jsonMap.contains(gSubscriptionAttrName) ? jsonMap[gSubscriptionAttrName].toString() : "";
