f2db1edcbfd296e6b0ea2e3408e5da3fde6e6e3b
maksym.tkachuk <maksymkei@gmail.com>
2025-06-17 21:33:33 +0300

    Update zhttpmanager.cpp

--- src/core/zhttpmanager.cpp
+++ src/core/zhttpmanager.cpp
@@ -2214,7 +2214,7 @@ public:
 							*/
 
 							// store response body
-							store_cache_response_buffer(subscriptionStr.toUtf8(), responseBuf_, QString(""));
+							//store_cache_response_buffer(subscriptionStr.toUtf8(), responseBuf_, QString(""));
 						}
 						else
 						{
