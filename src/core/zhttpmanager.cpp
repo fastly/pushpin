669b7bc3033c322803c6d8ea4d8b1f28751c2481
maksym.tkachuk <maksymkei@gmail.com>
2025-08-14 20:46:57 +0300

    Update zhttpmanager.cpp

--- src/core/zhttpmanager.cpp
+++ src/core/zhttpmanager.cpp
@@ -718,7 +718,7 @@ public:
 	void writeToClient_(const QByteArray &cacheItemId, const QByteArray &clientId, const QString &msgId, const QByteArray &instanceAddress, const QByteArray &instanceId)
 	{
 		assert(server_out_sock);
-		const char *logprefix = logPrefixForType(type);
+		const char *logprefix = logPrefixForType(CacheResponse);
 
 		QByteArray buf = load_cache_response_buffer(instanceAddress, cacheItemId, clientId, 0, msgId, instanceId, 0);
 
