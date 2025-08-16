5a3041825f9049692c2f3c4c76919a88f3a7e9dd
maksym.tkachuk <maksymkei@gmail.com>
2025-08-16 04:49:04 +0300

    Update zhttpmanager.cpp

--- src/core/zhttpmanager.cpp
+++ src/core/zhttpmanager.cpp
@@ -799,7 +799,40 @@ public:
 		QThread::usleep(1);
 	}
 
-	void writeToClient_(const QByteArray &cacheItemId, const QByteArray &clientId, const QString &msgId, const QByteArray &instanceAddress, const QByteArray &instanceId)
+	void writeToClient1_(const QByteArray &cacheItemId, const QByteArray &clientId, const QString &msgId, const QByteArray &instanceAddress, const QByteArray &instanceId)
+	{
+		assert(server_out_sock);
+
+		CacheItem* pCacheItem = load_cache_item(cacheItemId);
+
+		int newSeq = get_client_new_response_seq(clientId);
+
+		QByteArray data;
+		if (pCacheItem->proto == Scheme::http)
+		{
+			data = load_cache_response_buffer(instanceAddress, cacheItemId, clientId, newSeq, msgId, instanceId, 0);
+		}
+		else
+		{
+			data = load_cache_response_buffer(instanceAddress, cacheItemId, clientId, 0, msgId, instanceId, 0);
+		}
+
+		if (newSeq < 0)
+		{
+			log_debug("[WS] failed_ to get new response seq %s", clientId.constData());
+			return;
+		}
+
+		QByteArray buf = load_cache_response_buffer(instanceAddress, cacheItemId, clientId, newSeq, msgId, instanceId, 0);
+
+		// count methods
+		numMessageSent++;
+
+		server_out_sock->write(QList<QByteArray>() << buf);
+		QThread::usleep(1);
+	}
+
+	void writeToClient2_(const QByteArray &cacheItemId, const QByteArray &clientId, const QString &msgId, const QByteArray &instanceAddress, const QByteArray &instanceId)
 	{
 		assert(server_out_sock);
 		const char *logprefix = logPrefixForType(CacheResponse);
