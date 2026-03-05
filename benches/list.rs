5a5811d8abc3876301c64c014f93203a3ce8ab40
Justin Karneges <jkarneges@fastly.com>
2026-03-05 10:43:54 -0800

    update bench names

--- benches/list.rs
+++ benches/list.rs
@@ -21,13 +21,13 @@ use slab::Slab;
 use std::rc::Rc;
 
 fn criterion_benchmark(c: &mut Criterion) {
-    const NODE_COUNT: usize = 10000;
+    const NODE_KCOUNT: usize = 10;
 
     {
         // Preallocate the nodes memory
-        let mut nodes_slab = Some(Slab::with_capacity(NODE_COUNT));
+        let mut nodes_slab = Some(Slab::with_capacity(NODE_KCOUNT * 1000));
 
-        c.bench_function(&format!("index slab list push pop {NODE_COUNT}"), |b| {
+        c.bench_function(&format!("slab-push-pop-x{NODE_KCOUNT}k"), |b| {
             b.iter(|| {
                 let mut nodes = nodes_slab.take().unwrap();
                 let mut l = list::List::default();
@@ -54,9 +54,9 @@ fn criterion_benchmark(c: &mut Criterion) {
 
     {
         // Preallocate the nodes memory
-        let mut nodes_slab = Some(Slab::with_capacity(NODE_COUNT));
+        let mut nodes_slab = Some(Slab::with_capacity(NODE_KCOUNT));
 
-        c.bench_function(&format!("generic slab list push pop {NODE_COUNT}"), |b| {
+        c.bench_function(&format!("gen-slab-push-pop-x{NODE_KCOUNT}k"), |b| {
             b.iter(|| {
                 let mut nodes = nodes_slab.take().unwrap();
                 let mut l = list::SlabList::default();
@@ -82,23 +82,25 @@ fn criterion_benchmark(c: &mut Criterion) {
     }
 
     {
-        // Preallocate the nodes memory
-        let nodes_memory = Rc::new(memorypool::RcMemory::new(NODE_COUNT));
+        let node_count = NODE_KCOUNT * 1000;
 
-        c.bench_function(&format!("mp rc list push pop {NODE_COUNT}"), |b| {
+        // Preallocate the nodes memory
+        let nodes_memory = Rc::new(memorypool::RcMemory::new(node_count));
+
+        c.bench_function(&format!("mp-push-pop-x{NODE_KCOUNT}k"), |b| {
             b.iter(|| {
                 let mut b = list::RcBackend::default();
                 let mut l = list::GenericList::default();
 
                 let mut next_value: [u64; 32] = [0; 32];
-                while next_value[0] < NODE_COUNT as u64 {
+                while next_value[0] < node_count as u64 {
                     let n = list::RcNode::new(next_value, Some(&nodes_memory));
                     l.push_back(&mut b, n);
                     next_value[0] += 1;
                 }
 
                 let mut expected_value = 0;
-                while expected_value < NODE_COUNT as u64 {
+                while expected_value < node_count as u64 {
                     let n = l.pop_front(&mut b).unwrap();
                     assert_eq!(n.value()[0], expected_value);
                     expected_value += 1;
@@ -108,20 +110,22 @@ fn criterion_benchmark(c: &mut Criterion) {
     }
 
     {
-        c.bench_function(&format!("std rc list push pop {NODE_COUNT}"), |b| {
+        let node_count = NODE_KCOUNT * 1000;
+
+        c.bench_function(&format!("sys-push-pop-x{NODE_KCOUNT}k"), |b| {
             b.iter(|| {
                 let mut b = list::RcBackend::default();
                 let mut l = list::GenericList::default();
 
                 let mut next_value: [u64; 32] = [0; 32];
-                while next_value[0] < NODE_COUNT as u64 {
+                while next_value[0] < node_count as u64 {
                     let n = list::RcNode::new(next_value, None);
                     l.push_back(&mut b, n);
                     next_value[0] += 1;
                 }
 
                 let mut expected_value = 0;
-                while expected_value < NODE_COUNT as u64 {
+                while expected_value < node_count as u64 {
                     let n = l.pop_front(&mut b).unwrap();
                     assert_eq!(n.value()[0], expected_value);
                     expected_value += 1;
@@ -131,8 +135,10 @@ fn criterion_benchmark(c: &mut Criterion) {
     }
 
     {
+        let node_count = NODE_KCOUNT * 1000;
+
         // Preallocate the nodes
-        let nodes_memory = Rc::new(memorypool::RcMemory::new(NODE_COUNT));
+        let nodes_memory = Rc::new(memorypool::RcMemory::new(node_count));
         let mut nodes = Vec::new();
         let mut next_value: [u64; 32] = [0; 32];
         while nodes_memory.len() < nodes_memory.capacity() {
@@ -141,58 +147,54 @@ fn criterion_benchmark(c: &mut Criterion) {
             next_value[0] += 1;
         }
 
-        c.bench_function(
-            &format!("mp rc list push pop {NODE_COUNT} (preallocated nodes)"),
-            |b| {
-                b.iter(|| {
-                    let mut b = list::RcBackend::default();
-                    let mut l = list::GenericList::default();
+        c.bench_function(&format!("pre-mp-push-pop-x{NODE_KCOUNT}k"), |b| {
+            b.iter(|| {
+                let mut b = list::RcBackend::default();
+                let mut l = list::GenericList::default();
 
-                    for n in &nodes {
-                        l.push_back(&mut b, n.clone());
-                    }
+                for n in &nodes {
+                    l.push_back(&mut b, n.clone());
+                }
 
-                    let mut expected_value = 0;
-                    while expected_value < NODE_COUNT as u64 {
-                        let n = l.pop_front(&mut b).unwrap();
-                        assert_eq!(n.value()[0], expected_value);
-                        expected_value += 1;
-                    }
-                })
-            },
-        );
+                let mut expected_value = 0;
+                while expected_value < node_count as u64 {
+                    let n = l.pop_front(&mut b).unwrap();
+                    assert_eq!(n.value()[0], expected_value);
+                    expected_value += 1;
+                }
+            })
+        });
     }
 
     {
+        let node_count = NODE_KCOUNT * 1000;
+
         // Preallocate the nodes
         let mut nodes = Vec::new();
         let mut next_value: [u64; 32] = [0; 32];
-        while next_value[0] < NODE_COUNT as u64 {
+        while next_value[0] < node_count as u64 {
             let n = list::RcNode::new(next_value, None);
             nodes.push(n);
             next_value[0] += 1;
         }
 
-        c.bench_function(
-            &format!("std rc list push pop {NODE_COUNT} (preallocated nodes)"),
-            |b| {
-                b.iter(|| {
-                    let mut b = list::RcBackend::default();
-                    let mut l = list::GenericList::default();
+        c.bench_function(&format!("pre-sys-push-pop-x{NODE_KCOUNT}k"), |b| {
+            b.iter(|| {
+                let mut b = list::RcBackend::default();
+                let mut l = list::GenericList::default();
 
-                    for n in &nodes {
-                        l.push_back(&mut b, n.clone());
-                    }
+                for n in &nodes {
+                    l.push_back(&mut b, n.clone());
+                }
 
-                    let mut expected_value = 0;
-                    while expected_value < NODE_COUNT as u64 {
-                        let n = l.pop_front(&mut b).unwrap();
-                        assert_eq!(n.value()[0], expected_value);
-                        expected_value += 1;
-                    }
-                })
-            },
-        );
+                let mut expected_value = 0;
+                while expected_value < node_count as u64 {
+                    let n = l.pop_front(&mut b).unwrap();
+                    assert_eq!(n.value()[0], expected_value);
+                    expected_value += 1;
+                }
+            })
+        });
     }
 }
 
